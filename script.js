db429d582a2e930cf45fc8e2656956ce09cb64b3
Praveen Katikireddy <praveenk9417@gmail.com>
2023-03-23 22:28:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
+// get all panel elements
+const panels = document.querySelectorAll('.panel');
 
-<html>
+// add click event listener to each panel
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
-<head>
-	<link rel="stylesheet" href="styles.css">
-</head>
+// remove active class from all panels
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
 
-<body>
-	<!-- the entire body must be written by student -->
-	<div class="container">
-		<img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Explore the world" class="panel" id="panel-1">
-		<img src="https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Wild Forest" class="panel" id="panel-2">
-		<img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80" alt="City on Winter" class="panel" id="panel-3">
-		<img src="https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80" alt="Mountain Cloud" class="panel" id="panel-4">
-		<img src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Sunny Beach" class="panel" id="panel-5">
-	</div>
-    <script type="text/javascript" src="./script.js">
-</script>
-
-</body>
-
-</html>
+// set the first panel as active by default
+panels[0].classList.add('active');
